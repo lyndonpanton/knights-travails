afedb935667afafef7c068d0cbb9a8b284294f44
Lyndon Panton <LyndonPanton@gmail.com>
2025-10-02 23:13:49 +0100

    Setup: Configured images for webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 // console.log("Webpack configured successfully");
 
+import crowImage from "./asset/img/crow.jpg"
+
 import "./css/styles.css";
+
+const image = document.createElement("img");
+image.classList.add("image", "image-3");
+image.alt = "Crow 3";
+image.src = crowImage;
+
+document.getElementsByTagName("body")[0].appendChild(image);
