1f797786c51f4e329e35342b1b50c174ed42c7fc
Lyndon Panton <LyndonPanton@gmail.com>
2025-10-02 23:20:03 +0100

    Setup: Defined build, deploy, dev scripts

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import crowImage from "./asset/img/crow.jpg"
 
 import "./css/styles.css";
 
-const image = document.createElement("img");
-image.classList.add("image", "image-3");
-image.alt = "Crow 3";
-image.src = crowImage;
+// const image = document.createElement("img");
+// image.classList.add("image", "image-3");
+// image.alt = "Crow 3";
+// image.src = crowImage;
 
-document.getElementsByTagName("body")[0].appendChild(image);
+// document.getElementsByTagName("body")[0].appendChild(image);
